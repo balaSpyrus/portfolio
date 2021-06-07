0515334ae11a95e82def3ce397e46a21728ccb15
balasubramanian <bbalax.bbalax@gmail.com>
2021-06-07 20:27:39 +0530

    include contact details and sumary

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import { Avatar, ButtonGroup, Grid, IconButton, makeStyles, Theme, Typography } from '@material-ui/core';
-import { LinkedIn } from '@material-ui/icons';
+import { Avatar, ButtonGroup, Fade, Grid, IconButton, Link, makeStyles, Theme, Tooltip, Typography, withStyles } from '@material-ui/core';
+import { LinkedIn, EmailRounded, CallRounded, Facebook, Instagram } from '@material-ui/icons';
 import clsx from 'clsx';
 import React from 'react';
 import './App.scss';
+
+const LightTooltip = withStyles((theme: Theme) => ({
+  tooltip: {
+    backgroundColor: theme.palette.common.white,
+    boxShadow: theme.shadows[3],
+    padding: theme.spacing(2)
+  },
+}))(Tooltip);
 
 const useStyles = makeStyles((theme: Theme) => ({
   container: {
@@ -17,6 +25,24 @@ const useStyles = makeStyles((theme: Theme) => ({
     borderRadius: '5px',
     background: 'white',
     flexDirection: 'column'
+  },
+  summary: {
+  },
+  contact: {
+    marginTop: 'auto',
+
+    "&>*": {
+      display: 'flex',
+      alignItems: 'center',
+      gap: theme.spacing(1),
+      paddingBottom: theme.spacing(1),
+      "& svg": {
+        color: theme.palette.primary.main
+      },
+      "&:last-child": {
+        paddingBottom: 0
+      }
+    }
   },
   avatar: {
     width: theme.spacing(18),
@@ -36,19 +62,43 @@ function App() {
   return (
     <div className='app'>
       <Grid container className={classes.container}>
-        <Grid item container spacing={3} style={{  gap: 32, padding: 24 }}>
+        <Grid item container spacing={3} style={{ gap: 32, padding: 24 }}>
           <Grid container item sm={3} spacing={2} alignItems='center' direction='column' className={clsx(classes.mainSection, classes.infoSection)}>
             <Grid item>
               <Avatar alt="Balasubramanian Nagarajan" src="/asserts/images/profile.png"
                 className={classes.avatar} />
             </Grid>
             <Grid item>
-              <Typography variant='h6'>Balasubramanian Nagarajan</Typography>
-              <Typography variant='subtitle1'>Software Engineer III</Typography></Grid>
+              <Typography color='primary' variant='h6'>Balasubramanian Nagarajan</Typography>
+              <Typography color='textPrimary' variant='subtitle1'>Software Engineer III</Typography></Grid>
             <Grid item>
-              <ButtonGroup>
-                <IconButton> <LinkedIn /></IconButton>
+              <ButtonGroup color='primary'>
+                <IconButton href='https://www.linkedin.com/in/balasubramanian-nagarajan-9554438a/'> <LinkedIn /></IconButton>
+                <IconButton> <Facebook /></IconButton>
+                <IconButton> <Instagram /></IconButton>
               </ButtonGroup>
+            </Grid>
+            <Grid item className={classes.summary}>
+              <LightTooltip arrow interactive placement='right'
+                leaveDelay={400}
+                TransitionComponent={Fade} TransitionProps={{ timeout: 600 }}
+                title={
+                  <Typography variant='subtitle2' color='textSecondary' align='justify'>
+                    Creative Engineer offering 5 years of experience.
+                    Enthusiastic about developing forward thinking solutions to tomorrow's
+                    productivity problems. Resourceful and adaptable approach to challenges.
+                    Highly effective at developing new programs and fixing problems
+                    with existing systems.
+                </Typography>
+                }>
+                <Typography variant='subtitle2' color='primary' >
+                  Few words about me
+              </Typography>
+              </LightTooltip>
+            </Grid>
+            <Grid container item justify='space-between' className={classes.contact}>
+              <Link color='textSecondary' variant='subtitle2'><EmailRounded />bbalax.bbalax@gmail.com</Link>
+              <Link color='textSecondary' variant='subtitle2'><CallRounded />+91 9487680919</Link>
             </Grid>
           </Grid>
           <Grid container item sm={9} spacing={2} direction='column' className={classes.mainSection}>
